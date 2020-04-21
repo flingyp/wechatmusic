2665017643a9f7a53705b686edcec351bc552d35
flingPeng <1213563369@qq.com>
2020-04-21 19:36:50 +0800

    向数据库插入数据、数据去重

---
+++ cloudfunctions/getPlaylist/index.js
@@ -0,0 +1,52 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+
+cloud.init()
+
+const db = cloud.database()
+
+const palylistCollection = db.collection('playlist')
+
+const rp = require('request-promise')
+
+const URL = "http://musicapi.xiecheng.live/personalized"
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  // 读取 playlist 集合的数据
+  const list = await palylistCollection.get()
+
+  // 请求过来的 playlist 数据   
+  const playlist = await rp(URL).then(res => {
+    return JSON.parse(res).result
+  })
+
+  const newData = []
+  for(let i=0, len1 = playlist.length; i<len1; i++) {
+    let flag = true
+    for(let j=0, len2 = list.data.length; j<len2; j++) {
+      if(playlist[i].id === list.data[j].id) {
+        flag = false
+        break
+      }
+    }
+    if(flag) {
+      newData.push(playlist[i])
+    }
+  }
+
+  for(let i=0, len = newData.length; i<len; i++) {
+    await palylistCollection.add({
+      data: {
+        ...newData[i],
+        createTime: db.serverDate()
+      }
+    }).then(res => {
+      console.log('插入成功')
+    }).catch(err => {
+      console.error('插入失败')
+    })
+  }
+
+  return newData.length
+}
